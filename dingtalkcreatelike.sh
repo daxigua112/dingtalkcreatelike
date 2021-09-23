8a0aa62749780e51f944e033d7a9f5ddefc72479
z919126592 <81105770+z919126592@users.noreply.github.com>
2021-09-23 17:06:36 +0800

    删除cron

--- dingtalkcreatelike.sh
+++ dingtalkcreatelike.sh
@@ -1,6 +1,6 @@
 #Dingtalkcreatelike
 #Github https://github.com/z919126592/dingtalkcreatelike
-cron */1 * * * * task dingtalkcreatelike.sh
 curl "https://lv.dingtalk.com/interaction/createLike?uuid=$liveid&count=$likecount"
 echo "为$liveid直播间点赞了$likecount次赞"
 curl "https://api.day.app/$BARK_PUSH/钉钉点赞/为$liveid直播间点赞了$likecount次赞?group=dingtalkcreatelike"
+
