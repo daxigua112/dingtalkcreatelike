f6cf529034073b9c6c3762abbc3c2e1d9e965cf7
z919126592 <81105770+z919126592@users.noreply.github.com>
2021-09-23 16:46:09 +0800

    Create dingtalkcreatelike.sh

---
+++ dingtalkcreatelike.sh
@@ -0,0 +1,5 @@
+#Dingtalkcreatelike
+#Github https://github.com/z919126592/dingtalkcreatelike
+curl "https://lv.dingtalk.com/interaction/createLike?uuid=$liveid&count=$likecount"
+echo "为$liveid直播间点赞了$likecount次赞"
+curl "https://api.day.app/$BARK_PUSH/钉钉点赞/为$liveid直播间点赞了$likecount次赞?group=dingtalkcreatelike"
